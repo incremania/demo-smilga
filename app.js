2fdf5deed37eb5bf35df352fcff089e50d59139a
okikijesuFalade <companychris00@gmail.com>
2023-12-28 00:37:26 +0100

    adding files

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+const express = require('express');
+const path = require('path')
+const app = express();
+
+// app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+    res.json({"hello": "world"})
+})
+
+app.all('*', (req, res) => {
+    res.send('route not available')
+})
+
+app.listen(5000, () => {
+    console.log('listening on port 5000');
+})
